a903355bcaaae7aa9f9333c1776e5618c1c5324c
bass18598 <35221213+bass18598@users.noreply.github.com>
2018-02-02 15:53:37 +0530

    count6.c

---
+++ count6.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,j=0,k=0,l;
+char s[100];
+scanf("%c",&s[i]);
+l=strlen(a);
+for(i=0;i<l;i++)
+{
+if(((s[i]>='0')&&(s[i]<='9'))||((s[i]>='A')&&(s[i]<='Z'))||((s[i]>='a')&&(s[i]<='z')))
+{
+j=j;
+}
+else
+{
+k=++k;
+}
+}
+printf("%d",k);
+return 0;
+}
