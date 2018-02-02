b430b8be2f3dba2f79a6282322736b03910010aa
bass18598 <35221213+bass18598@users.noreply.github.com>
2018-02-02 15:37:12 +0530

    count5.c

---
+++ count5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+ char s[500];
+ int i,count=0;
+ printf("\n Enter the string");
+ scanf("%d",&s);
+ for(i=0;str[i]!='\0';i++)
+ {
+  if(s[i]>='0'&&s[i]<=9)
+  {
+   count++;
+   }
+   printf("%d", count);
+   }
+   
